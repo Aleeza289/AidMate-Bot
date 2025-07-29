bd1362fbc547a292ba7902b6d4162295c2ad6503
Aleeza289 <aleezarazzaq28@gmail.com>
2025-07-29 14:23:52 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import base64
 
 # ========================== Config ==========================
 st.set_page_config(page_title="Emergency First-Aid Assistant", layout="centered", page_icon="🩺")
-API_KEY = "API_Key"
+API_KEY = "api_key"
 MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"
 JSON_FILE = "data.json"
 
